6434a5015a5a71f7507b1c877963375d3d4ee8b9
Ayush Ray <rayayush@gmail.com>
2023-11-06 17:02:47 +0530

    Adding FEAT

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 //INPUT BUTTON// && Display f elements
 let myLeads = []
 let inputBtn= document.getElementById("input-btn")
+let deleteBtn = document.getElementById("delete-btn")
+let saveBtm = document.getElementById("save-btn")
 const  inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    render()
-    
-})
-
-function render(){
+function render(leads){
     let listItem =""
-    for(i = 0; i < myLeads.length;i++){
+    for(i = 0; i < leads.length;i++){
         listItem += `
                     <li>
                         <a targe='_blank' href='
-                        ${myLeads[i]}'>${myLeads[i]}
+                        ${leads[i]}'>${leads[i]}
                         </a>
                     </li>
                     `
@@ -27,6 +26,36 @@ function render(){
     
     ulEl.innerHTML = listItem 
 }
+
+const tabs =[
+    {url: "aaaaaaaaaaaaaaaa"}
+]
+
+/*SAVE TAB BUTTON */
+saveBtm.addEventListener("click",function(){
+    let xyz = tabs[0].url
+    console.log(xyz)
+})
+
+
+/*DELETE BUTTON*/
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads);
+})
+
+/*SAVE INPUT BUTTON*/
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads);
+    console.log(localStorage.getItem("myLeads"))
+})
+console.log("leadsFromLocalStorage")
+
+
 
 
 
